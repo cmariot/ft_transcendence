7cecb890029e41f77a4722a6a3570eadaca4bccc
Charles Mariot <contact@charles-mariot.fr>
2023-01-14 23:33:06 +0100

    Delete app/backend/srcs/dist directory

--- app/backend/srcs/dist/users/controllers/users.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { UsersService } from "../services/users.service";
-import { UserEntity } from "../entity/user.entity";
-export declare class UsersController {
-    private readonly userService;
-    constructor(userService: UsersService);
-    getUsers(): Promise<UserEntity[]>;
-}
--- app/backend/srcs/dist/users/services/users.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from "typeorm";
-import { UserEntity } from "../entity/user.entity";
-export declare class UsersService {
-    private userRepository;
-    constructor(userRepository: Repository<UserEntity>);
-    saveUser(user: any): Promise<UserEntity>;
-    getUsers(): Promise<UserEntity[]>;
-    getByUsername(username: string): Promise<UserEntity | undefined>;
-}
