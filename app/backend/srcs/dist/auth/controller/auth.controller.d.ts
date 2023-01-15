07238fdfd753ea331f753b9cc1547e71f384ac51
Charles Mariot <contact@charles-mariot.fr>
2023-01-15 01:44:51 +0100

    Delete auth.controller.d.ts

--- app/backend/srcs/dist/auth/controller/auth.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthService } from "../service/auth.service";
-export declare class AuthController {
-    private authService;
-    constructor(authService: AuthService);
-    forty_two(): void;
-    forty_two_redirect(req: any, res: any): void;
-    test(): string;
-    logout(res: any): void;
-}
