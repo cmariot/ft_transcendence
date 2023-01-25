45481e4cd0a6537757efa79a3d7015fd12f54dac
Charles Mariot <contact@charles-mariot.fr>
2023-01-26 00:36:40 +0100

    YES

---
+++ app/frontend/srcs/src/auth/login/Login42.tsx
@@ -0,0 +1,17 @@
+function Login42() {
+	function login42() {
+		try {
+			window.open("https://localhost:8443/api/auth/42", "_self");
+		} catch (e) { console.error(e); }
+
+	}
+
+	return (
+		<div>
+			<h2>Login with your 42 account</h2>
+			<button onClick={login42}>Login</button>
+		</div>
+	)
+}
+
+export default Login42;
