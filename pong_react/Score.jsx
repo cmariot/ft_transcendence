a104768f9ae3bc95b9b167020dee6e6de2b3ef6f
Aimen BENSETTI <abensett@e2r2p7.clusters.42paris.fr>
2023-02-20 14:53:32 +0100

    angles bug to be fixed, to add winner screen

---
+++ pong_react/Score.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+export class Score extends React.Component {
+
+     render() {
+        return (
+           <div className={this.props.position}>
+              <h2>Player {this.props.player}</h2>
+              <h1>{this.props.total}</h1>
+           </div>
+        );
+     }
+    }
+
+
+export class Winner extends React.Component {
+
+      render() {
+            return (
+               <div className={this.props.position}>
+                  <h3>Player {this.props.player} WON THE GAME</h3>
+               </div>
+            );
+      }
+     }
+
