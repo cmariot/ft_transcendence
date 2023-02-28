d4a478c2a7fb88cbd78ca4fc58a51b05a916162d
Francesco LEE <flee@e1r5p9.clusters.42paris.fr>
2023-02-28 09:44:48 +0100

    fix: can't set private message channel name(DTO problem)

--- app/backend/srcs/src/chat/dtos/newChannel.dto.ts
+++ app/backend/srcs/src/chat/dtos/newChannel.dto.ts
@@ -1,47 +1,60 @@
 import { IsEmpty, IsNotEmpty, IsString, MinLength } from "class-validator";
 
 export class ProtectedChannelDTO {
-    @IsNotEmpty()
-    @IsString()
-    channelName: string;
+  @IsNotEmpty()
+  @IsString()
+  channelName: string;
 
-    @IsNotEmpty()
-    @IsString()
-    channelType: string;
+  @IsNotEmpty()
+  @IsString()
+  channelType: string;
 
-    @IsString()
-    @MinLength(10)
-    channelPassword: string;
+  @IsString()
+  @MinLength(10)
+  channelPassword: string;
 
-    @IsEmpty()
-    channelOwner: string;
+  @IsEmpty()
+  channelOwner: string;
 }
 export class PrivateChannelDTO {
-    @IsNotEmpty()
-    @IsString()
-    channelName: string;
+  @IsNotEmpty()
+  @IsString()
+  channelName: string;
 
-    @IsNotEmpty()
-    @IsString()
-    channelType: string;
+  @IsNotEmpty()
+  @IsString()
+  channelType: string;
 
-    @IsString()
-    @IsEmpty()
-    channelPassword: string;
+  @IsString()
+  @IsEmpty()
+  channelPassword: string;
 
-    @IsEmpty()
-    channelOwner: string;
+  @IsEmpty()
+  channelOwner: string;
+}
+
+export class PrivateMessageDTO {
+  @IsNotEmpty()
+  @IsString()
+  channelName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  channelType: string;
+
+  @IsEmpty()
+  channelOwner: string;
 }
 
 export class PublicChannelDTO {
-    @IsNotEmpty()
-    @IsString()
-    channelName: string;
+  @IsNotEmpty()
+  @IsString()
+  channelName: string;
 
-    @IsNotEmpty()
-    @IsString()
-    channelType: string;
+  @IsNotEmpty()
+  @IsString()
+  channelType: string;
 
-    @IsEmpty()
-    channelOwner: string;
+  @IsEmpty()
+  channelOwner: string;
 }
