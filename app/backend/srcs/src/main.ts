1658d304371bcf9283d478ca1e057e8afae44464
Charles Mariot (42) <contact@charles-mariot.fr>
2023-03-01 16:50:09 +0100

    bye bye helmet

--- app/backend/srcs/src/main.ts
+++ app/backend/srcs/src/main.ts
@@ -3,13 +3,11 @@ import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
 import { ValidationPipe } from "@nestjs/common";
-import helmet from "helmet";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     app.use(cookieParser());
-    app.use(helmet());
     app.enableCors({
         origin: process.env.HOST,
         credentials: true,
