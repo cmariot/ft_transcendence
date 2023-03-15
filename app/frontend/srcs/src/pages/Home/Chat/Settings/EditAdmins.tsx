718f949a3292542759ca8d9ff8f1c8b87ca21a7c
Charles Mariot <contact@charles-mariot.fr>
2023-03-15 17:08:06 +0100

    Fix admins list

--- app/frontend/srcs/src/pages/Home/Chat/Settings/EditAdmins.tsx
+++ app/frontend/srcs/src/pages/Home/Chat/Settings/EditAdmins.tsx
@@ -1,11 +1,70 @@
-import { ChangeEvent, useContext, useState } from "react";
+import { ChangeEvent, useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { ChatContext } from "../../../../contexts/ChatProvider";
+import { SocketContext } from "../../../../contexts/SocketProvider";
+import { UserContext } from "../../../../contexts/UserProvider";
 
 const EditAdmins = () => {
     const chat = useContext(ChatContext);
+    const socket = useContext(SocketContext);
+    const user = useContext(UserContext);
     const [newAdminName, setNewAdmin] = useState("");
     const [update, setUpdate] = useState(false);
+
+    // When a admin is removed
+    useEffect(() => {
+        async function updateAdmins(data: {
+            username: string;
+            channel: string;
+        }) {
+            if (chat.channel === data.channel) {
+                if (
+                    chat.isChannelAdmin === true ||
+                    chat.isChannelOwner === true
+                ) {
+                    let admins = chat.admins;
+                    let index = admins.findIndex(
+                        (admins) => admins === data.username
+                    );
+                    if (index !== -1) {
+                        admins.splice(index, 1);
+                        chat.setAdmins(admins);
+                    }
+                }
+                if (data.username === user.username) {
+                    try {
+                        const connectResponse = await axios.post(
+                            "/api/chat/connect",
+                            { channelName: data.channel }
+                        );
+                        if (connectResponse.status === 201) {
+                            chat.setisChannelAdmin(
+                                connectResponse.data.channel_admin
+                            );
+                            chat.setAdmins(connectResponse.data.channel_admins);
+                            chat.setmutedUsers(
+                                connectResponse.data.muted_users
+                            );
+                            chat.setbannedUsers(
+                                connectResponse.data.banned_users
+                            );
+                        }
+                    } catch (connectResponse: any) {
+                        console.log(connectResponse.response.data.message);
+                    }
+                }
+            }
+            setUpdate((prevState) => !prevState);
+        }
+        socket.on("chat.remove.admin", updateAdmins);
+        return () => {
+            socket.off("chat.remove.admin", updateAdmins);
+        };
+    }, [chat, socket, user]);
+
+    useEffect(() => {
+        setUpdate((prevState) => !prevState);
+    }, [chat.admins]);
 
     function handleNewsAdminChange(event: ChangeEvent<HTMLInputElement>) {
         event.preventDefault();
